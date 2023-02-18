5ed3b436ee2b6e7ff0b33f96e547c33bef6aa36a
subhankarroy612 <subhankarroy612@gmail.com>
2023-02-18 14:03:13 +0530

    pagination done

--- frontend/src/pages/UserDetails.jsx
+++ frontend/src/pages/UserDetails.jsx
@@ -9,33 +9,52 @@ import {
   TableCaption,
   TableContainer,
   Text,
+  Image,
+  Button,
 } from '@chakra-ui/react';
 import styles from '../styles/userdetails.module.css'
 import { url } from '../components/url';
 import axios from 'axios'
-
+import { RxThickArrowLeft, RxThickArrowRight } from 'react-icons/rx'
 
 
 export default function UserDetails() {
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
-    getData().then((r) => setData(r))
-  }, []);
+    getData(page).then((r) => setData(r))
+  }, [page]);
 
 
-  const getData = async () => {
-    let res = await axios(url + '/fetchUsers/userdetails');
+  const getData = async (page = 1) => {
+    let res = await axios(url + '/fetchUsers/userdetails?page=' + page);
     return res.data
   }
+
+  const handlePage = (v) => {
+    setPage(page + v)
+  }
+
+  console.log(page)
 
   return (
     <div id={styles.userdetails}>
 
+      <section id={styles.functionalities}>
+        <Button isDisabled={page === 1} bg={'teal'} onClick={() => handlePage(-1)}>
+          <RxThickArrowLeft color='white' fontSize={'40px'} />
+        </Button>
+        <Text as={'i'} fontSize={'2xl'}>Page: {page}</Text>
+        <Button bg={'teal'} onClick={() => handlePage(1)}>
+          <RxThickArrowRight color='white' fontSize={'40px'} />
+        </Button>
+      </section>
+
       <TableContainer id={styles.user_table}>
         <Table variant='striped' colorScheme='teal'>
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <TableCaption>User details</TableCaption>
           <Thead>
             <Tr>
               <Th>Picture</Th>
@@ -49,20 +68,14 @@ export default function UserDetails() {
             {
               data && data.map((ele, i) => {
                 return <Tr key={i}>
-                  <Td>  <img src={ele.picture.large} alt='' /> </Td>
-                  <Td>{ele.name.title}. {ele.name.first} {ele.name.last}</Td>
+                  <Td>  <Image borderRadius={'8px'} src={ele.picture.large} alt='' /> </Td>
+                  <Td><Text as={'b'}>{ele.name.title}. {ele.name.first} {ele.name.last}</Text></Td>
                   <Td><Text as={'b'}>{ele.email}</Text></Td>
-                  <Td>millimetres (mm)</Td>
+                  <Td><Text as={'b'}>{ele.location.country}</Text></Td>
                 </Tr>
               })
             }
-            <Tr>
-              <Td>inches</Td>
-              <Td>millimetres (mm)</Td>
-              <Td>millimetres (mm)</Td>
-              <Td>millimetres (mm)</Td>
-              {/* <Td isNumeric>25.4</Td> */}
-            </Tr>
+
 
           </Tbody>
 
